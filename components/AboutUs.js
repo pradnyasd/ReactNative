eedd6477fcfc9ccc9d921061048047916d7d4c3b
pradnyad <pradnya24.dalvi@gmail.com>
2019-08-19 21:51:45 +0530

    using redux

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -3,6 +3,14 @@ import { View, Text, ScrollView, Title, FlatList, Image } from 'react-native';
 import { Card } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 import {ListItem} from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      leaders: state.leaders
+    }
+  }
 
 function History() {
   return(
@@ -14,12 +22,6 @@ function History() {
 }
 
 class About extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      leaders: LEADERS
-    };
-  }
 
   static navigationOptions = {
     title: 'About Us'
@@ -34,8 +36,7 @@ class About extends Component {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
-                    leftAvatar={{ source: require('./images/alberto.png')}}
-                  />
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}                  />
         );
     };
 
@@ -45,9 +46,9 @@ class About extends Component {
       <ScrollView>
         <History />
         <Card title="Corporate Leadership">
-            <FlatList
-                data={this.state.leaders}
-                renderItem={renderleader}
+        <FlatList
+                data={this.props.leaders.leaders}
+                renderItem={renderLeader}
                 keyExtractor={item => item.id.toString()}
                 />
         </Card>
@@ -58,4 +59,4 @@ class About extends Component {
 
 
 }
-export default About;
+export default connect(mapStateToProps)(About);
